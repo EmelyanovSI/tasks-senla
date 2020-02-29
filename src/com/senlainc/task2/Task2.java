5ccb30fc2e22196bd7c271e844012b885dd2a232
EmelyanovSI <xemelyanov@gmail.com>
2020-03-01 02:05:30 +0300

    senla - task2 - gcd, lcm and print methods

--- src/com/senlainc/task2/Task2.java
+++ src/com/senlainc/task2/Task2.java
@@ -2,4 +2,19 @@ package com.senlainc.task2;
 
 public class Task2 {
 
+    public static int gcd(int a, int b) {
+        return a == 0 ? b : gcd(b % a, a);
+    }
+
+    public static int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
+    }
+
+    public static void print(int a, int b) {
+        System.out.println(String.format(
+                "gcd(%d, %d) = %d\nlcm(%d, %d) = %d",
+                a, b, gcd(a, b), a, b, lcm(a, b)
+        ));
+    }
+
 }
