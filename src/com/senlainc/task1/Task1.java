68cdb10804b90edb6789d307c2c5b9cc1144ab62
EmelyanovSI <xemelyanov@gmail.com>
2020-03-01 01:02:33 +0300

    senla - task1 - isEven, isPrime and print methods

--- src/com/senlainc/task1/Task1.java
+++ src/com/senlainc/task1/Task1.java
@@ -1,5 +1,23 @@
 package com.senlainc.task1;
 
+import java.math.BigInteger;
+
 public class Task1 {
+
+    public static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+    public static boolean isPrime(int number) {
+        return number > 1 && BigInteger.valueOf(number).isProbablePrime((int) Math.log(number));
+    }
+
+    public static void print(int number) {
+        System.out.println(String.format(
+                number + " is %s and %s number",
+                isEven(number) ? "even" : "odd",
+                isPrime(number) ? "prime" : "composite"
+        ));
+    }
 
 }
