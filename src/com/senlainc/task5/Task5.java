25dde9f9f1aa48974c769c11ecb1fb6981f86bb9
EmelyanovSI <xemelyanov@gmail.com>
2020-03-01 04:43:42 +0300

    senla - task5 - isPalindrome, findPalindromes and print methods

--- src/com/senlainc/task5/Task5.java
+++ src/com/senlainc/task5/Task5.java
@@ -1,5 +1,25 @@
 package com.senlainc.task5;
 
+import java.util.ArrayList;
+
 public class Task5 {
+
+    public static boolean isPalindrome(String string) {
+        return string.equals(new StringBuilder().append(string).reverse().toString());
+    }
+
+    public static ArrayList<Integer> findPalindromes(int number) {
+        ArrayList<Integer> array = new ArrayList<Integer>();
+        for (int i = 0; i < number; ++i) {
+            if (isPalindrome(String.valueOf(i))) {
+                array.add(i);
+            }
+        }
+        return array;
+    }
+
+    public static void print(int number) {
+        System.out.println("All palindromes:\n" + findPalindromes(number));
+    }
 
 }
