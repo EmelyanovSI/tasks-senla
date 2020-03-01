a93ece23aac34fbb483aa7d088aadad3af6ad254
EmelyanovSI <xemelyanov@gmail.com>
2020-03-01 03:55:10 +0300

    senla - task4 - count and print methods

--- src/com/senlainc/task4/Task4.java
+++ src/com/senlainc/task4/Task4.java
@@ -2,4 +2,20 @@ package com.senlainc.task4;
 
 public class Task4 {
 
+    public static int count(String string, String word) {
+        int number = 0;
+        String[] words = string.split("[ :;,.!?()\"']+");
+        word = word.toLowerCase();
+        for (String w : words) {
+            if (word.equals(w.toLowerCase())) {
+                ++number;
+            }
+        }
+        return number;
+    }
+
+    public static void print(String string, String word) {
+        System.out.println(count(string, word) + " matches found");
+    }
+
 }
