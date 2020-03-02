801e5bbf05b7950c386244d6b2f0a3d3d199ec64
EmelyanovSI <xemelyanov@gmail.com>
2020-03-02 03:48:32 +0300

    senla - task5 - warning fixed

--- src/com/senlainc/task5/Task5.java
+++ src/com/senlainc/task5/Task5.java
@@ -9,7 +9,7 @@ public class Task5 {
     }
 
     public static ArrayList<Integer> findPalindromes(int number) {
-        ArrayList<Integer> array = new ArrayList<Integer>();
+        ArrayList<Integer> array = new ArrayList<>();
         for (int i = 0; i < number; ++i) {
             if (isPalindrome(String.valueOf(i))) {
                 array.add(i);
